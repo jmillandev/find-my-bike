08ab464c7958e451a09a8b4691ebba220dc3be36
Jesús Millán <jgmc3012@gmail.com>
2024-02-29 16:15:44 -0400

    :star: CsvStationRepository Red Test Cases

---
+++ test/src/stations/infrastructure/repositories/csv.spec.ts
@@ -0,0 +1,49 @@
+import { Station } from '../../../../../src/stations/domain/entity';
+import { StationRepository } from '../../../../../src/stations/domain/repository';
+import { StationCoordinates } from '../../../../../src/stations/domain/value_objects/coordinates';
+import { StationDistance } from '../../../../../src/stations/domain/value_objects/distance';
+import { StationLocation } from '../../../../../src/stations/domain/value_objects/location';
+import { StationName } from '../../../../../src/stations/domain/value_objects/name';
+import {
+  StationStatus,
+  Statuses,
+} from '../../../../../src/stations/domain/value_objects/status';
+import { CsvStationRepository } from '../../../../../src/stations/infrastructure/repositories/csv';
+
+describe('CsvStationRepository', () => {
+  let repository: StationRepository;
+
+  beforeEach(async () => {
+    repository = new CsvStationRepository('./storage/mibici_2024_01.csv');
+  });
+
+  it('should return an empty list of stations', async () => {
+    const coors = {
+      latitude: 10.5082346,
+      longitude: -66.9218495,
+    };
+    const coordinates = new StationCoordinates(coors);
+    const distance = new StationDistance(1000);
+    const stations = await repository.findNearby(coordinates, distance);
+
+    expect(stations).toEqual([]);
+  });
+
+  it('should return a list of stations', async () => {
+    const expected_station = new Station(
+      new StationName('(GDL-002) C. Colonias  / Av.  Ni�os h�roes'),
+      new StationCoordinates({ latitude: 20.667228, longitude: -103.366 }),
+      new StationStatus(Statuses.ACTIVE),
+      new StationLocation('POL�GONO CENTRAL'),
+    );
+    const coordinates = new StationCoordinates({
+      latitude: 20.666378,
+      longitude: -103.34882,
+    });
+
+    const distance = new StationDistance(1800);
+    const stations = await repository.findNearby(coordinates, distance);
+
+    expect(stations).toContain(expected_station);
+  });
+});
